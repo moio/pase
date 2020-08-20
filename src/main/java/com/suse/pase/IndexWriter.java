ab7a44beaf3924cc287e4d2d98a85f43732f7fef
Silvio Moioli <moio@suse.com>
2020-08-20 17:07:41 +0200

    Refactoring: extract SourceAnalyzer

--- src/main/java/com/suse/pase/IndexWriter.java
+++ src/main/java/com/suse/pase/IndexWriter.java
@@ -2,8 +2,6 @@ package com.suse.pase;
 
 import static org.apache.lucene.index.IndexWriterConfig.OpenMode.CREATE;
 
-import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.util.CharTokenizer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.StringField;
@@ -62,23 +60,5 @@ public class IndexWriter implements AutoCloseable {
         this.writer.close();
     }
 
-    /** Divides text by line (treats the whole line as a term). */
-    private class SourceCodeTokenizer extends CharTokenizer {
-        public SourceCodeTokenizer() {
-            super(DEFAULT_TOKEN_ATTRIBUTE_FACTORY, 1024);
-        }
 
-        @Override
-        protected boolean isTokenChar(int cp) {
-            return cp != 0x0D && cp != 0x0A;
-        }
-    }
-
-    /** Wraps a SourceCodeTokenizer and nothing else */
-    private class SourceAnalyzer extends Analyzer {
-        @Override
-        protected TokenStreamComponents createComponents(String fieldName) {
-            return new TokenStreamComponents(new SourceCodeTokenizer());
-        }
-    }
 }
