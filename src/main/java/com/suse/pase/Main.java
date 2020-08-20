486ae0bf78d77a22de23bc9f01ab84b79e9f3441
Silvio Moioli <moio@suse.com>
2020-08-20 16:26:55 +0200

    Main: add minimal commandline parser

--- src/main/java/com/suse/pase/Main.java
+++ src/main/java/com/suse/pase/Main.java
@@ -1,18 +1,25 @@
-import com.suse.pase.DirectoryWalker;
-import com.suse.pase.IndexWriter;
+package com.suse.pase;
 
 import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        if (args.length != 3){
+            printHelp();
+        }
 
-        var sourcePath = Path.of(".");
-        var indexPath = Path.of("index");
+        var sourcePath = Path.of(args[1]);
+        var indexPath = Path.of(args[2]);
 
         try (var writer = new IndexWriter(indexPath)) {
             DirectoryWalker.forEachTextFileIn(sourcePath, path -> {
                 writer.add(path);
             });
         }
+    }
+
+    private static void printHelp() {
+        System.out.println("Usage: java -jar pase.jar index <source_path> <index_path>");
+        System.exit(-1);
     }
 }
