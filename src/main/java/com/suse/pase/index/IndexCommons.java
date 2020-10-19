f9f7c06126992f1b74bdfa7a35613a1d782f0e41
Silvio Moioli <moio@suse.com>
2020-10-19 14:56:02 +0200

    Refactoring: treat IndexCommons as internal to package

--- src/main/java/com/suse/pase/index/IndexCommons.java
+++ src/main/java/com/suse/pase/index/IndexCommons.java
@@ -3,15 +3,15 @@ package com.suse.pase.index;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.util.CharTokenizer;
 
-public class IndexCommons {
-    public static final String SOURCE_FIELD = "source";
-    public static final String PATH_FIELD = "path";
-    public static final String PATH_FINGERPRINT_FIELD = "path_fingerprint";
-    public static final String LAST_UPDATED_FIELD = "last_updated";
+class IndexCommons {
+    static final String SOURCE_FIELD = "source";
+    static final String PATH_FIELD = "path";
+    static final String PATH_FINGERPRINT_FIELD = "path_fingerprint";
+    static final String LAST_UPDATED_FIELD = "last_updated";
 
 
     /** Divides text by line (treats the whole line as a term). */
-    public static class SourceAnalyzer extends Analyzer {
+    static class SourceAnalyzer extends Analyzer {
         private class SourceCodeTokenizer extends CharTokenizer {
             public SourceCodeTokenizer() {
                 super(DEFAULT_TOKEN_ATTRIBUTE_FACTORY, 1024);
